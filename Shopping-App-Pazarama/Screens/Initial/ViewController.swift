04c7c0c9fbee5ec93fa5ba6c29f493b0a0f4fbca
aniloncul <aniloncul@gmail.com>
2022-10-26 18:22:59 +0300

    initial commit

---
+++ Shopping-App-Pazarama/Screens/Initial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Shopping-App-Pazarama
+//
+//  Created by Anıl Öncül on 26.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
